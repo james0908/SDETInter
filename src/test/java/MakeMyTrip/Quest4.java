272577c3eceade91ae8494705d4f045d94bb3596
Rashmi <rashmis.sharma31@gmail.com>
2025-05-21 12:23:57 +0530

    Adding project

--- src/test/java/MakeMyTrip/Quest4.java
+++ src/test/java/MakeMyTrip/Quest4.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.Test;//testNg
 
 import java.time.Duration;
 
